34a46681d1c295bffda05ea375341e17df073777
Jack O'Hara <jack@jackohara.io>
2021-12-01 22:27:43 +0000

    Created a dynamically-sized board

---
+++ src/board.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let cells = 4;
+</script>
+
+<div class="game-of-life-board">
+  {#each new Array(cells) as _}
+    <div class="game-of-life-cell" />
+  {/each}
+</div>
+
+<style>
+  .game-of-life-board {
+    width: 90%;
+    height: 85%;
+    align-items: center;
+    border-radius: 4px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+  }
+
+  .game-of-life-board .game-of-life-cell {
+    border: 1px solid grey;
+    height: 100%;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Board from '../board.svelte';
+
+  let columns = 2;
+  let rows = 2;
+  let cells = 4;
+
+  function onChange(newVal: number, identifier: 'rows' | 'columns') {
+    const element = document.querySelector('.game-of-life-board') as HTMLElement;
+
+    element.style.setProperty(`grid-template-${identifier}`, `repeat(${newVal}, 1fr)`);
+    cells = rows * columns;
+  }
+</script>
+
+<main>
+  <h1>Conway's Game of Life</h1>
+
+  <div class="grid-size-inputs-container">
+    <label for="grid-size-rows">Rows</label>
+    <label for="grid-size-columns">Columns</label>
+    <input
+      id="grid-size-rows"
+      type="number"
+      required={true}
+      bind:value={rows}
+      on:change={() => onChange(rows, 'rows')}
+    />
+    <input
+      id="grid-size-columns"
+      type="number"
+      required={true}
+      bind:value={columns}
+      on:change={() => onChange(columns, 'columns')}
+    />
+  </div>
+
+  <Board {cells} />
+</main>
+
+<style>
+  :global(body) {
+    background-color: lightblue;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  main {
+    height: 95vh;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+  }
+
+  .grid-size-inputs-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 0.5em;
+  }
+
+  .grid-size-inputs-container input {
+    width: 35%;
+  }
+
+  .grid-size-inputs-container > * {
+    margin-inline: auto;
+  }
+</style>
